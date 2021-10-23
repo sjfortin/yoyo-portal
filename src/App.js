a337d0530a076f5ce92df408bdf75d9701c120ec
Sam Fortin <sam.fortin@irishtitan.com>
2021-10-23 11:13:19 -0500

    init yoyo portal

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
+import { Button, Box, Heading, Stack, Text, Container } from "@chakra-ui/react";
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
-    <div className="mainContainer">
-
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
-
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
-      </div>
-    </div>
+    <Box w="100vw" h="100vh" bgGradient="linear(to-r, blue.200, pink.500)" py={6}>
+      <Container maxW="xl" centerContent>
+        <Stack spacing={6}>
+          <Heading as="h1" size="4xl">
+            It is yoyo time! <span role="img">🪀</span>
+          </Heading>
+          <Text fontSize="2xl">
+            Send me a YOYO! Connect your Ethereum wallet and yoyo it up!
+          </Text>
+          <Button onClick={wave} colorScheme="blue">
+            YOYO
+          </Button>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
